9b182d0596b1a7a71039ec3f1d3f20554b536687
mikegreene <mgreene11111@yahoo.com>
2020-11-07 10:23:36 -0800

    created Lab12

---
+++ data/books.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id int,
+    author varchar(255),
+    title varchar(255),
+    isbn varchar(255),
+    image_url varchar(255),
+    descr varchar(255)
+);
